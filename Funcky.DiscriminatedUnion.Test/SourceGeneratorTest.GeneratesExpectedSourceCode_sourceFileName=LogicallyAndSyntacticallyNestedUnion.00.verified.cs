bc192e804cf8eac7ebf8f82e931aed6f0528030f
Mathias Fischler <mathias.fischler@messerli.ch>
2024-09-17 15:49:25 +0200

    Update verified test cases

--- Funcky.DiscriminatedUnion.Test/SourceGeneratorTest.GeneratesExpectedSourceCode_sourceFileName=LogicallyAndSyntacticallyNestedUnion.00.verified.cs
+++ Funcky.DiscriminatedUnion.Test/SourceGeneratorTest.GeneratesExpectedSourceCode_sourceFileName=LogicallyAndSyntacticallyNestedUnion.00.verified.cs
@@ -14,6 +14,9 @@ namespace Funcky
         /// <summary>Generates exhaustive <c>Match</c> and <c>Switch</c> methods for the entire type hierarchy.</summary>
         public bool Flatten { get; set; }
 
+        /// <summary>If a specialized partition extension method for <c>IEnumerable<YourType></c> should be generated. Defaults to false.</summary>
+        public bool GeneratePartitionExtension { get; set; }
+
         /// <summary>Customized the generic type name used for the result in the generated <c>Match</c> methods. Defaults to <c>TResult</c>.</summary>
         public string? MatchResultTypeName { get; set; }
     }
